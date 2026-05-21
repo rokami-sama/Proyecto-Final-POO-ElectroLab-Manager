16748917246ed56511c4e5c820665650c6cf3809
rokami-sama <rodrigosamaniego1986@gmail.com>
2026-05-20 23:48:39 -0300

    Cree la Clase Padre y agregue sus propiedades iniciales, tambien los metodos de agregar, retirar y mostrar Info del Stock

--- src/Inventario/ComponentesElectronicos.java
+++ src/Inventario/ComponentesElectronicos.java
@@ -9,5 +9,66 @@ package Inventario;
  * @author rodri
  */
 public class ComponentesElectronicos {
+    private String nombre;
+    private String codigo;
+    private int stock;
+    private double voltaje;
+
+    public ComponentesElectronicos(String nombre, String codigo, int stock, double voltaje) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.stock = stock;
+        this.voltaje = voltaje;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public double getVoltaje() {
+        return voltaje;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setVoltaje(double voltaje) {
+        this.voltaje = voltaje;
+    }
+    
+    public void agregarStock(){
+        
+    } 
+    
+    public void retirarStock(){
+        
+    }
+    
+    public String mostrarInfo(){
+        
+    }
+
+    @Override
+    public String toString() {
+        return "ComponentesElectronicos{" + "nombre=" + nombre + ", codigo=" + codigo + ", stock=" + stock + ", voltaje=" + voltaje + '}';
+    }
+    
     
 }
