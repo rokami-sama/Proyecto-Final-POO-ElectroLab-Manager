c0d671e57502067d2baa4ae5e9096e4e2cd5cdbf
rokami-sama <rodrigosamaniego1986@gmail.com>
2026-05-22 09:51:41 -0300

    Agregue y deje en comentario un posible metodo a agregar de eliminar componente buscado

--- src/ComponentesElectronicos/Inventario.java
+++ src/ComponentesElectronicos/Inventario.java
@@ -17,7 +17,17 @@ public class Inventario implements Serializable{
     public void agregarComponente(ComponentesElectronicos c){
         componentes.add(c);
     }
-
+    
+    /*Public void eliminarComponente(String codigo) throws ComponenteNoEncontradoException{
+        for(ComponentesElectricos c: componentes){
+            if(c.getCodigo().equalsIgnoreCase(codigo)){
+                c.remove();
+            }
+        }
+        throws new ComponenteNoEncontradoExcepcition();
+    }
+    */
+    
     public ComponentesElectronicos buscarComponente(String codigo) throws ComponenteNoEncontradoException{
         for(ComponentesElectronicos c: componentes){
             if(c.getCodigo().equalsIgnoreCase(codigo)){
