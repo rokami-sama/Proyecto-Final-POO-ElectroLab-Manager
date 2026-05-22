06ebeb5fd28c6584eccb83a91f9b66f4373c3eb3
rokami-sama <rodrigosamaniego1986@gmail.com>
2026-05-22 10:27:43 -0300

    Agregue los metodos de persistencia cargar y guardar para el inventario

--- src/Persistencia/PersistenciaInventario.java
+++ src/Persistencia/PersistenciaInventario.java
@@ -11,11 +11,27 @@ import java.io.*;
  *
  * @author rodri
  */
-public class PersistenciaInventario implements {
-    public void guardar(){
-        
+public class PersistenciaInventario {
+    
+    public static void guardar(Inventario inventario){
+        try {
+            ObjectOutputStream salida= new ObjectOutputStream(new FileOutputStream("Inventario.dat",true));
+            salida.writeObject(inventario);
+            salida.close();
+        } catch (IOException e) {
+            System.out.println("Error al guardar  el archivo.");
+        }
     }
-    public void cargar(){
-        
+    
+    public static Inventario cargar(){
+        try {
+            ObjectInputStream entrada= new ObjectInputStream(new FileInputStream("Inventario.dat"));
+            Inventario inventario=(Inventario) entrada.readObject();
+            entrada.close();
+            return inventario;
+        } catch (IOException e) {
+            //System.out.println("Error al cargar el archivo.");
+            return new Inventario();
+        }
     }
 }
