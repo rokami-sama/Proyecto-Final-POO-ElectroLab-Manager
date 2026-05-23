e547d661cd2dc471b7de521dd7f505a02b8b31ae
rokami-sama <rodrigosamaniego1986@gmail.com>
2026-05-22 21:22:56 -0300

    Agregue el menu interactivo, con sus opciones aun que falta mas

--- src/Main/ElectroLabManager.java
+++ src/Main/ElectroLabManager.java
@@ -4,6 +4,7 @@
  */
 package Main;
 
+import java.io.*;
 import Excepctions.*;
 import ComponentesElectronicos.*;
 import Persistencia.*;
@@ -26,6 +27,52 @@ public class ElectroLabManager {
         Inventario inventario= PersistenciaInventario.cargar();
         ArrayList<Usuario> usuarios= PersistenciaUsuarios.cargar();
         ArrayList<Prestamo> prestamo= PersistenciaPrestamos.cargar();
+        
+        int opcion;
+        
+        do{
+            System.out.println("------------BIENVENIDO A ELECTROLAB MANAGER----------");
+            System.out.println("1.Registrar Usuario");
+            System.out.println("2.Agregar Componente");
+            System.out.println("3.Prestar Componente");
+            System.out.println("4.Salir");
+            
+            opcion=sc.nextInt();
+            sc.nextLine();
+            
+            switch(opcion){
+                case 1://Registrar Usuario
+                    System.out.println("a.Estudiante");
+                    System.out.println("b.Docente");
+                    char profesion;
+                    profesion=sc.next().charAt(0);
+                    switch(profesion){
+                        case 'a':
+                            System.out.println("Nombre: ");
+                            System.out.println("ID: ");
+                            System.out.println("Carrera: ");
+                            System.out.println("Semestre: ");
+                            
+                            Estudiante estudiante=new Estudiante(nombre, id, carrera, opcion);
+                            break;
+                        case 'b':
+                            
+                            break;
+                    }
+                    System.out.println("/nUsuario: ");
+                    usuarios=sc.nextLine();
+                    Usuario usuario=new Usuario(nombre, id) {
+                    
+                    }
+                case 2:
+                    
+                case 3:
+                    
+                case 4:
+                    
+            }
+            
+        }
         
         
         
